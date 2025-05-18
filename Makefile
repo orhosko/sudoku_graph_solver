c29dd1ff7f4e18db91616656c78e3019213fd6e8
orhosko <yilmazor21@itu.edu.tr>
2025-05-18 10:52:37 +0300

    Init

---
+++ Makefile
@@ -0,0 +1,2 @@
+main: main2.cpp
+	g++ -o main main2.cpp --std=c++20 -g
---
+++ main.cpp
@@ -0,0 +1,145 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+struct Square {
+    // int x;
+    // int y;
+    int index;
+    int value; // color
+};
+
+// TODO: make constexpr
+
+std::array<std::array<Square, 4>, 4> board{
+    std::array<Square, 4>{Square{0, 0}, Square{1, 1}, Square{2, -1},
+                          Square{3, 3}},
+    std::array<Square, 4>{Square{4, -1}, Square{5, 2}, Square{6, -1},
+                          Square{7, 0}},
+    std::array<Square, 4>{Square{8, 2}, Square{9, -1}, Square{10, 0},
+                          Square{11, 1}},
+    std::array<Square, 4>{Square{12, 3}, Square{13, 0}, Square{14, -1},
+                          Square{15, -1}}};
+
+std::array<std::array<int, 16>, 16> adjMatrix{};
+
+void create_row_deps(std::array<std::array<int, 16>, 16> &adjMartix) {
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (i % 4 != j) {
+                adjMatrix[i][(i / 4) * 4 + j] += 1;
+            }
+        }
+    }
+}
+
+void create_col_deps(std::array<std::array<int, 16>, 16> adjMartix) {
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (i / 4 != j) {
+                adjMatrix[i][(i % 4) + j * 4] += 1;
+            }
+        }
+    }
+}
+
+void create_block_deps(std::array<std::array<int, 16>, 16> adjMartix) {
+    for (int i = 0; i < 16; ++i) {
+        int x = i % 4;
+        int y = i / 4;
+        int block_index = (y / 2) * 2 + (x / 2);
+
+        int block_min = (block_index % 2) * 2 + (block_index / 2) * 8;
+
+        // std::cout << "block_index: " << block_index << std::endl;
+        // std::cout << "block_min: " << block_min << std::endl;
+
+        adjMatrix[i][block_min] += 1;
+        adjMatrix[i][block_min + 1] += 1;
+        adjMatrix[i][block_min + 4] += 1;
+        adjMatrix[i][block_min + 5] += 1;
+
+        adjMatrix[i][i] -= 1;
+    }
+}
+
+void normalize_adj_matrix(std::array<std::array<int, 16>, 16> &adjMatrix) {
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 16; ++j) {
+            adjMatrix[i][j] = std::min(adjMatrix[i][j], 1);
+        }
+    }
+}
+
+void greedy_coloring(std::array<std::array<Square, 4>, 4> &board,
+                     std::array<std::array<int, 16>, 16> &adjMatrix) {
+    std::array<int, 16> values;
+    std::fill(values.begin(), values.end(), -1);
+
+    for (int i = 0; i < 16; ++i) {
+        std::array<bool, 4> available;
+        std::fill(available.begin(), available.end(), true);
+
+        for (int j = 0; j < 16; ++j) {
+            if (adjMatrix[i][j] == 1 && values[j] != -1) {
+                available[values[j]] = false;
+            }
+        }
+
+        int cr;
+        for (cr = 0; cr < 4; ++cr) {
+            if (available[cr]) {
+                break;
+            }
+        }
+
+        if (cr == 4) {
+            std::cout << "Greedy failed. No available color for square " << i << std::endl;
+            return;
+        }
+
+        values[i] = cr;
+    }
+
+    for (int i = 0; i < 16; ++i) {
+        board[i / 4][i % 4].value = values[i];
+    }
+}
+
+int main() {
+    create_row_deps(adjMatrix);
+    create_col_deps(adjMatrix);
+    create_block_deps(adjMatrix);
+
+    normalize_adj_matrix(adjMatrix);
+
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 16; ++j) {
+            std::cout << adjMatrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "===========================" << std::endl;
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("%2d ", board[i][j].value);
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "===========================" << std::endl;
+
+    // solve by coloring
+    greedy_coloring(board, adjMatrix);
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("%2d ", board[i][j].value);
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
